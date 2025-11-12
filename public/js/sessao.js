6ae5f90f65ac0052c0cda311540cd6ccf1db7991
0tavio4ugusto <otavio.rodrigues@sptech.school>
2025-11-11 22:24:05 -0300

    Cadastro e Login funcionando e cadastrando no banco(retirada a validação de aquarios do WEB-DATA-Viz)

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -2,13 +2,15 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nick = sessionStorage.NICK_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+    var id = sessionStorage.ID_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nick != null) {
         b_usuario.innerHTML = nick;
     } else {
-        window.location = "../login.html";
+        window.location = "../meulogin.html";
     }
 }
 
